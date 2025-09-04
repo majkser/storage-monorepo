30a7f63242e4c65257b25b9b7cc007ce011d5542
Mikołaj Kowalski <mikser.kowalski@gmail.com>
2025-09-04 23:22:49 +0200

    fix: removed duplicate of mobile menu

--- apps/web/app/(main)/dashboard/page.tsx
+++ apps/web/app/(main)/dashboard/page.tsx
@@ -1,13 +1,9 @@
-import { UploadButton } from "@/components/dashboard/upload-button";
-import { MobileMenu } from "@/components/sidebar/mobile-menu";
-import UploadedFiles from "@/components/dashboard/uploadedFiles";
+import { UploadButton } from '@/components/dashboard/upload-button';
+import UploadedFiles from '@/components/dashboard/uploadedFiles';
 
 export default function DashboardPage() {
   return (
     <>
-      <div className="p-4 md:hidden">
-        <MobileMenu />
-      </div>
       <div className="p-6 pt-0 md:pt-6 w-full max-w-[1200px] mx-auto flex flex-col gap-8 sm:flex-row justify-between items-center mb-4">
         <div>
           <h1 className="text-white h2">Uploaded Files</h1>
