af897bf9b909989e2671c89396c950e8d226825c
Mikołaj Kowalski <mikser.kowalski@gmail.com>
2025-09-05 19:40:15 +0200

    fix: added connecton realease in the model

--- apps/api/src/models/fileAccessModel.ts
+++ apps/api/src/models/fileAccessModel.ts
@@ -9,13 +9,15 @@ export async function createFileAccess(FileAccess: FileAccess): Promise<void> {
   const connection = await dbConnection.getConnection();
 
   try {
-    connection.execute(
+    await connection.execute(
       'INSERT INTO fileAccess (fileId, userId) VALUES (?, (SELECT id FROM users WHERE email = ?))',
       [FileAccess.fileId, FileAccess.email]
     );
   } catch (error) {
     console.error('Error creating file access:', error);
     throw error;
+  } finally {
+    connection.release();
   }
 }
 
@@ -33,6 +35,8 @@ export async function getFileAccessByUserEmail(
   } catch (error) {
     console.error('Error retrieving file access:', error);
     throw error;
+  } finally {
+    connection.release();
   }
 }
 
@@ -47,5 +51,7 @@ export async function deleteFileAccess(FileAccess: FileAccess): Promise<void> {
   } catch (error) {
     console.error('Error deleting file access:', error);
     throw error;
+  } finally {
+    connection.release();
   }
 }
